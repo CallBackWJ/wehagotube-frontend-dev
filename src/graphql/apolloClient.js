350f013b9d3d2188e967cbc71aa94829a3d122e2
최원진 <>
2019-10-15 13:21:45 +0900

    웹소켓 경로 변경

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -4,7 +4,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from 'apollo-utilities';
 const wsLink = new WebSocketLink({
-  uri: 'https://wehagotube-backend-dev.herokuapp.com/',
+  uri: 'ws://wehagotube-backend-dev.herokuapp.com/',
   //uri: "ws://localhost:4000/",
   options: {
     reconnect: true
