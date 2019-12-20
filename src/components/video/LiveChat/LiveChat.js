f6bde69479f0bb3c1cdfcf4e076d026f07652fc8
최원진 <>
2019-12-20 11:10:30 +0900

    재빌드를 위한 푸쉬

--- src/components/video/LiveChat/LiveChat.js
+++ src/components/video/LiveChat/LiveChat.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {VIDEO} from "../../../graphql/video";
-import { useQuery } from "react-apollo-hooks";
+import { VIDEO } from '../../../graphql/video';
+import { useQuery } from 'react-apollo-hooks';
 const LiveChat = props => {
-    const { data, error, loading ,refetch} = useQuery(VIDEO, {
-        variables: { schedule_id: props.id }
-      });
-    
-      if (loading) return ""
-      if (error) return "error";
-      const videoId=(data.video)?data.video.youtubeId:"";
-      const src="https://www.youtube.com/live_chat?v="+videoId+"&embed_domain=wehagotube-frontend-dev.netlify.com"
-    
-    return (
-        <iframe
-        allowfullscreen=""
-        frameborder="0"
-        height="100%"
-        src={src}
-        width="100%"
-      ></iframe>
-    );
+  const { data, error, loading, refetch } = useQuery(VIDEO, {
+    variables: { schedule_id: props.id }
+  });
+
+  if (loading) return '';
+  if (error) return 'error';
+  const videoId = data.video ? data.video.youtubeId : '';
+  const src =
+    'https://www.youtube.com/live_chat?v=' +
+    videoId +
+    '&embed_domain=wehagotube-frontend-dev.netlify.com';
+
+  return (
+    <iframe
+      allowfullscreen=""
+      frameborder="0"
+      height="100%"
+      src={src}
+      width="100%"
+    ></iframe>
+  );
 };
 
-LiveChat.propTypes = {
-    
-};
+LiveChat.propTypes = {};
 
 export default LiveChat;
